ec917f817a944a35f9a8894ed7cf07c078320193
mikeg64 <mikeg64@993b1211-408a-0410-8964-a8528df93632>
2012-10-23 09:00:56 +0000

    Corrections to the OZT code for the single GPU test

--- include/iosac2.5dparams.h
+++ include/iosac2.5dparams.h
@@ -10,7 +10,7 @@ int ngk=2;
 #ifdef USE_SAC
 //vac ozt
 int ni;
-ni=122;    //OZT tests
+ni=252;    //OZT tests
 ni=ni+2*ngi;
 //ni=512;
 //real xmax = 6.2831853;  
@@ -23,7 +23,7 @@ real dx = xmax/(ni);
 // Define the y domain
 #ifdef USE_SAC
 //vac ozt
-int nj = 122;  //OZT tests
+int nj = 252;  //OZT tests
 //int nj=2;  //BW test
 nj=nj+2*ngj;
 //nj=512;
@@ -68,15 +68,13 @@ int finishsteering=0;
 //char *cfgfile="zero1.ini";
 
 //char *cfgfile="zero1_np020203.ini";
-//char *cfgfile="zero1_ot_asc_np0201.ini";
-//char *cfgfile="zero1_np0202_asc.ini";
+//char *cfgfile="zero1_np0201.ini";
 char *cfgfile="zero1_ot_asc.ini";
 //char *cfgfile="zero1_BW_bin.ini";
 //char *cfgout="zero1_np010203."
-//char *cfgout="out/zeroOT";
-char *cfgout="tmpout/zero1_.out";
-char *cfggathout="out/zero1_.out";
-//char *cfgout="zero1_np0202.out";
+char *cfgout="out/zeroOT";
+//char *cfgout="zero1_np0201.out";
+
 
 
 
@@ -91,7 +89,7 @@ dt=0.0002;  //OZT test
 //nt=5000;
 //nt=200000;
 //nt=150000;
-nt=5;
+nt=220;
 
 
 real *t=(real *)calloc(nt,sizeof(real));
@@ -150,9 +148,9 @@ p->divbon=0.0;
 p->divbfix=0.0;
 p->hyperdifmom=1.0;
 p->readini=1.0;
-p->cfgsavefrequency=1;
-p->noghost=0;
-p->fullgridini=1;
+p->cfgsavefrequency=10;
+
+
 p->xmax[0]=xmax;
 p->xmax[1]=ymax;
 p->nt=nt;
@@ -180,13 +178,11 @@ p->chyp[rho]=0.02;
 
 
 
-#ifdef USE_MULTIGPU
+#ifdef USE_MPI
 //number of procs in each dim mpi only
 p->pnpe[0]=2;
-p->pnpe[1]=2;
+p->pnpe[1]=1;
 p->pnpe[2]=1;
-
-p->npe=4;
 #endif
 
 
